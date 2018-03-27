1599674c50353a7be137e2648b2068ead66ee3e5
Jack Reuter <djreuter45@gmail.com>
2018-03-27 15:49:43 -0600

    modified display

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -7,7 +7,10 @@ namespace MvcMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
